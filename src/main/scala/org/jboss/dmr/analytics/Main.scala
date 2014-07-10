5bfd5a215bc3ed01e0d561949101f2024dd61895
Harald Pehl <harald.pehl@gmail.com>
2014-07-10 13:14:17 +0200

    Add server info

--- src/main/scala/org/jboss/dmr/analytics/Main.scala
+++ src/main/scala/org/jboss/dmr/analytics/Main.scala
@@ -4,22 +4,59 @@ import java.io.{BufferedWriter, FileWriter}
 
 import com.rockymadden.delimited.Write.DelimitedWriter
 import com.typesafe.scalalogging.slf4j.Logger
+import org.jboss.dmr.repl.Response._
+import org.jboss.dmr.repl.{Response, Client}
 import org.jboss.dmr.repl.Client._
+import org.jboss.dmr.scala._
 import org.slf4j.LoggerFactory
 
 object Main {
 
+  val logger = Logger(LoggerFactory.getLogger("org.jboss.dmr.analytics.Main"))
+
   def main(args: Array[String]) {
-
-    val logger = Logger(LoggerFactory.getLogger("org.jboss.dmr.analytics.Main"))
-
-    // read attributes
-    val con = ("localhost", 9990)
-    logger.info(s"Reading attributes from $con")
+    // if present take host and port from the command line
+    val con = if (args.length == 2)
+      (args(0), args(1).toInt)
+    else
+      ("localhost", 9990)
     val client = connect(con._1, con._2)
-    val sequencer = new Sequencer(client)
-    val allAttributes = sequencer.read()
+
+    readServerInfo(client)
+    readAttributes(client)
+
     client.close()
+  }
+
+  def readServerInfo(client: Client) {
+    client ! (ModelNode() at root op 'read_resource('include_runtime -> true)) map {
+      case Response(Success, result) =>
+
+        // Read release and management version
+        val serverInfo = IndexedSeq(
+            result.get("release-codename") flatMap (_.asString) getOrElse "n/a",
+            result.get("release-version") flatMap (_.asString) getOrElse "n/a",
+            result.get("process-type") flatMap (_.asString) getOrElse "n/a",
+            result.get("management-major-version") flatMap (_.asString) getOrElse "n/a",
+            result.get("management-micro-version") flatMap (_.asString) getOrElse "n/a",
+            result.get("management-minor-version") flatMap (_.asString) getOrElse "n/a")
+
+        val releaseFilename = "output/server.csv"
+        logger.info(s"Writing CSV to $releaseFilename")
+        DelimitedWriter.using(new BufferedWriter(new FileWriter(releaseFilename))) {
+          writer =>
+            writer.writeLine(Some(IndexedSeq("name", "version", "process-type", "major", "micro", "minor")))
+            writer.writeLine(Some(serverInfo))
+        }
+
+      case Response(Failure, failure) =>
+        val error = failure.asString getOrElse "undefined"
+        logger.error(s"Error reading version: $error")
+    }
+  }
+
+  def readAttributes(client: Client) {
+    val allAttributes = new Sequencer(client).read()
 
     // filter attributes
     def any[A](predicates: (A => Boolean)*): A => Boolean =
